58fd082bfc0bf902bb5622953e37490eba8cdb89
Laura Abbott <laura@labbott.name>
2019-12-06 18:37:24 -0500

    Add the URL, remove some bad scripts

--- _posts/add_layout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in `ls *.markdown`; do
-	echo -en "---\nlayout: post\n" > /tmp/a
-	tail -n+2 $i >> /tmp/a
-	mv /tmp/a $i
-done
--- _posts/fix_category.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in `ls *.markdown`; do
-	sed -i .bak 's/categories:/category:/' $i
-done
